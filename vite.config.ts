e1cafaa987aae7a144a2e08f120eb6a03efdd6ea
유민지 <yyoumi4854@gmail.com>
2022-11-06 17:06:58 +0900

    문제되는 router 라이브러리 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
-import { reactRouterPlugin } from 'vite-plugin-next-react-router';
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRouterPlugin()],
+  plugins: [react()],
 })
